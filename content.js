b343158fffab4dfe3db441e261a15449afc93f44
Jonathan <jtc7537@gmail.com>
2019-01-03 15:20:19 -0500

    removed scripts

--- content.js
+++ content.js
@@ -1,8 +1,1 @@
-// alert('asdf')
-const script = `
-<script type='text/babel'>
-const message = () => 'Hello World';
-document.getElementsByTagName('body')[0].innerHTML(message())
-</script>
-` 
-$('head').append(script)
+console.log('asdf')
